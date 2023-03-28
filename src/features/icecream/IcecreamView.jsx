c3dd199fa436c48331da8828b7e6b95bcea16771
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-03-28 14:55:08 +0200

    use the state to display values in components and use local state to restock values via a user input

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,13 +1,18 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "../icecream/icecreamSlice";
 
 const IcecreamView = () => {
+    const [value, setValue] = useState(1);
     const numOfIcecreams = useSelector(state => state.icecream.numOfIcecreams);
+    const dispatch = useDispatch();
 
     return (
         <div>
             <h2>Number of ice creams - {numOfIcecreams}</h2>
-            <button>Order ice creams</button>
-            <button>Restock ice creams</button>
+            <button onClick={() => dispatch(ordered())}>Order ice creams</button>
+            <input type="number" value={value} onChange={e => setValue(parseInt(e.target.value))} />
+            <button onClick={() => dispatch(restocked(value))}>Restock ice creams</button>
         </div>
     );
 }
